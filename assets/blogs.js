cb283e4cadda6b807ce241426112f2fda955ac1d
Khusan <bahodirovkhusan@gmail.com>
2024-05-02 21:13:35 +0500

    finished featured blog section

---
+++ assets/blogs.js
@@ -0,0 +1,11 @@
+let blogSwiper = new Swiper(".blogSwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
