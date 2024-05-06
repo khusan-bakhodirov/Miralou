16f8db92d20890985e05746ac44d4ad88371b9d5
Khusan <bahodirovkhusan@gmail.com>
2024-05-06 16:39:12 +0500

    convert add to cart into ajax api call

--- assets/product.js
+++ assets/product.js
@@ -6,11 +6,71 @@ const images = document.querySelectorAll(".product_img");
 const overlay = document.querySelector(".overlay");
 const close_btn = document.querySelector(".close");
 const product_variants = document.querySelectorAll(".product_variant");
+const price = document.querySelector('.product_exact-price')
+const compare_price = document.querySelector('.prodcut_compare-price');
+const product_form  = document.querySelector('.product_form');
+const product_quantity = document.querySelector('[name=quantity]')
+const product_variant = document.querySelector('[name=id]');
+const cart_modal = document.querySelector('.cart_modal');
+const cart_modal_close = document.querySelector('.modal_close');
+const product_btn = document.querySelector('.product_btn');
+const cart_count = document.querySelector('.cart_count');
+// handle product add
+product_form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  product_btn.disabled = true;
+  product_btn.querySelector('.product_btn-text').style.display = 'none';
+  product_btn.querySelector('.button_spinner').style.display = 'block';
+  const form_data  = new FormData(product_form);
+  form_data.append('quantity', product_quantity.value);
+  form_data.append('id', product_variant.value);
+  const res = await  fetch('/cart/add.js', {
+    method: 'POST',
+    body: form_data,
+    headers: {
+      'X-Requested-With': 'XMLHttpRequest',
+    },
+  })
+  const cart_res = await fetch('/cart.js');
+  const cart = await cart_res.json();
+  console.log(cart)
+  const data = await res.json();
+  if(data) {
+    product_btn.disabled = false;
+    product_btn.querySelector('.product_btn-text').style.display = 'block';
+    product_btn.querySelector('.button_spinner').style.display = 'none';
+    cart_modal.classList.add('active');
+    cart_modal.querySelector('.cart_item-img').src = data.featured_image.url
+    cart_modal.querySelector('.item_title').textContent = data.title
+    cart_modal.querySelector('.item_variant').textContent = `Size: ${data.variant_title}`;
+    cart_count.textContent = `View cart (${cart.item_count})`;
+  }
+})
+
+cart_modal_close.addEventListener('click', () => {
+  cart_modal.classList.remove('active');
+})
+
+
+function formatPrice(price, currency = '$') {
+    // Convert price to a string and pad with zeros if necessary
+    let priceString = String(price).padStart(3, '0');
+
+    // Insert a decimal point two positions from the end
+    priceString = priceString.slice(0, -2) + '.' + priceString.slice(-2);
+
+    // Return the formatted price with currency symbol
+    return currency + priceString;
+}
+
+// handle description toggling 
 
 description_btn.addEventListener("click", () => {
   product_description.classList.toggle("active");
   btn_icon.classList.toggle("active");
 });
+
+// handle img modal
 
 images.forEach((image) => {
   image.addEventListener("click", () => {
@@ -30,6 +90,8 @@ overlay.addEventListener("click", () => {
   overlay.classList.remove("active");
 });
 
+
+// handle product variants
 product_variants.forEach((variant, index) => {
   if (index == 0) {
     variant.querySelector("input").setAttribute("name", "id");
@@ -46,9 +108,12 @@ product_variants.forEach((variant) => {
     });
     variant.querySelector("input").setAttribute("name", "id");
     variant.classList.add("selected_variant");
+    price.textContent = formatPrice(variant.getAttribute('data-price'));
+    compare_price.textContent = formatPrice( variant.getAttribute('data-compare'));
   });
 });
 
+// SWIPER JS
 let swiper = new Swiper(".mobile_product-gallery", {
   slidesPerView: 1,
 
