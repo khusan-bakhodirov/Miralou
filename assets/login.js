c089a533b250246f59070c62e4c5221a2570b1ce
Khusan <bahodirovkhusan@gmail.com>
2024-05-18 15:54:27 +0500

    finish login and register page

---
+++ assets/login.js
@@ -0,0 +1,4 @@
+document.querySelector('.login_forgot-password').addEventListener('click', () => {
+    document.querySelector('.login').classList.add('hidden');
+    document.querySelector('.forgot_password').classList.remove('hidden');
+})
